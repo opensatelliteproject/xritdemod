f9d5e10a44e326f6823bbfdf94f38bd7ab852939
Lucas Teske <lucas@teske.net.br>
2017-01-31 02:53:38 -0200

    Decoupled HRIT/LRIT configurations from defines to config file

---
+++ demodulator/src/Parameters.h
@@ -0,0 +1,65 @@
+/*
+ * Parameters.h
+ *
+ *  Created on: 31/01/2017
+ *      Author: Lucas Teske
+ */
+
+#ifndef SRC_PARAMETERS_H_
+#define SRC_PARAMETERS_H_
+
+#define Q(x) #x
+#define QUOTE(x) Q(x)
+
+// These are the parameters used by the demodulator. Change with care.
+
+// GOES HRIT Settings
+#define HRIT_CENTER_FREQUENCY 1694100000
+#define HRIT_SYMBOL_RATE      927000
+#define HRIT_RRC_ALPHA        0.3f
+
+// GOES LRIT Settings
+#define LRIT_CENTER_FREQUENCY 1691000000
+#define LRIT_SYMBOL_RATE      293883
+#define LRIT_RRC_ALPHA        0.5f
+
+// Loop Settings
+#define LOOP_ORDER 2
+#define RRC_TAPS 63
+#define PLL_ALPHA 0.025
+#define CLOCK_ALPHA 0.0037
+#define CLOCK_MU 0.5
+#define CLOCK_OMEGA_LIMIT 0.005
+#define CLOCK_GAIN_OMEGA (CLOCK_ALPHA * CLOCK_ALPHA) / 4.0
+#define AGC_RATE 0.01
+#define AGC_REFERENCE 0.5
+#define AGC_GAIN 1
+#define AGC_MAX_GAIN 4000
+
+#define AIRSPY_MINI_DEFAULT_SAMPLERATE 3000000
+#define AIRSPY_R2_DEFAULT_SAMPLERATE   2500000
+#define DEFAULT_SAMPLE_RATE AIRSPY_MINI_DEFAULT_SAMPLERATE
+#define DEFAULT_DECIMATION 1
+
+#define DEFAULT_LNA_GAIN 5
+#define DEFAULT_VGA_GAIN 5
+#define DEFAULT_MIX_GAIN 5
+
+// FIFO Size in Samples
+// 1024 * 1024 samples is about 4Mb of ram.
+// This should be more than enough
+#define FIFO_SIZE (1024 * 1024)
+
+// Config parameters
+#define CFG_SYMBOL_RATE "symbolRate"
+#define CFG_FREQUENCY "frequency"
+#define CFG_RRC_ALPHA "rrcAlpha"
+#define CFG_MODE "mode"
+#define CFG_SAMPLE_RATE "sampleRate"
+#define CFG_DECIMATION "decimation"
+#define CFG_AGC "agcEnabled"
+#define CFG_MIXER_GAIN "mixerGain"
+#define CFG_LNA_GAIN "lnaGain"
+#define CFG_VGA_GAIN "vgaGain"
+
+#endif /* SRC_PARAMETERS_H_ */
--- demodulator/src/demodulator.cpp
+++ demodulator/src/demodulator.cpp
@@ -21,37 +21,7 @@
 using namespace OpenSatelliteProject;
 using namespace SatHelper;
 
-#define USE_HRIT
-
-// HRIT Options
-#ifdef USE_HRIT
-#	define CENTER_FREQUENCY 1694100000
-#	define SYMBOL_RATE 927000
-#	define BASE_DECIMATION 1
-#	define RRC_ALPHA 0.3
-#else
-// LRIT Options
-#	define CENTER_FREQUENCY 1691000000
-#	define SYMBOL_RATE 293883
-// Use 2 for 2.5Msps on Airspy R2 and 3.0Msps on Airspy Mini
-#	define BASE_DECIMATION 2
-#	define RRC_ALPHA 0.5
-#endif
-#define LOOP_ORDER 2
-#define RRC_TAPS 63
-#define PLL_ALPHA 0.025
-#define CLOCK_ALPHA 0.0037
-#define CLOCK_MU 0.5
-#define CLOCK_OMEGA_LIMIT 0.005
-#define CLOCK_GAIN_OMEGA (CLOCK_ALPHA * CLOCK_ALPHA) / 4.0
-#define AGC_RATE 0.01
-#define AGC_REFERENCE 0.5
-#define AGC_GAIN 1
-#define AGC_MAX_GAIN 4000
-
-// FIFO Size in Samples
-// 1024 * 1024 samples is about 4Mb of ram.
-#define FIFO_SIZE (1024 * 1024)
+#include "Parameters.h"
 
 
 AGC *agc;
@@ -61,6 +31,8 @@ FirFilter *rrcFilter;
 FirFilter *decimator;
 SymbolManager symbolManager;
 SampleFIFO samplesFifo(FIFO_SIZE);
+
+int baseDecimation;
 
 bool running;
 
@@ -122,7 +94,7 @@ void processSamples() {
 	samplesFifo.unsafe_unlockMutex();
 
 	// Decimation / Lowpass
-	length /= BASE_DECIMATION;
+	length /= baseDecimation;
 	decimator->Work(buffer0, buffer1, length);
 
 	// Automatic Gain Control
@@ -148,24 +120,136 @@ void symbolLoopFunc() {
 	}
 }
 
+void setLRITMode(ConfigParser &parser) {
+	parser[CFG_SYMBOL_RATE] = std::string(QUOTE(LRIT_SYMBOL_RATE));
+	parser[CFG_MODE] = std::string("lrit");
+	parser[CFG_RRC_ALPHA] = std::string(QUOTE(LRIT_RRC_ALPHA));
+	parser[CFG_FREQUENCY] = std::string(QUOTE(LRIT_CENTER_FREQUENCY));
+	parser[CFG_SAMPLE_RATE] = std::string(QUOTE(DEFAULT_SAMPLE_RATE));
+	parser[CFG_DECIMATION] = std::string(QUOTE(DEFAULT_DECIMATION));
+}
+
+void setHRITMode(ConfigParser &parser) {
+	parser[CFG_SYMBOL_RATE] = std::string(QUOTE(HRIT_SYMBOL_RATE));
+	parser[CFG_MODE] = std::string("hrit");
+	parser[CFG_RRC_ALPHA] = std::string(QUOTE(HRIT_RRC_ALPHA));
+	parser[CFG_FREQUENCY] = std::string(QUOTE(HRIT_CENTER_FREQUENCY));
+	parser[CFG_SAMPLE_RATE] = std::string(QUOTE(DEFAULT_SAMPLE_RATE));
+	parser[CFG_DECIMATION] = std::string(QUOTE(DEFAULT_DECIMATION));
+}
+
+void setDefaults(ConfigParser &parser) {
+	setLRITMode(parser);
+	parser[CFG_AGC] = "true";
+	parser[CFG_LNA_GAIN] = std::string(QUOTE(DEFAULT_LNA_GAIN));
+	parser[CFG_MIXER_GAIN] = std::string(QUOTE(DEFAULT_MIX_GAIN));
+	parser[CFG_VGA_GAIN] = std::string(QUOTE(DEFAULT_VGA_GAIN));
+	parser.SaveFile();
+}
+
 int main(int argc, char **argv) {
+	ConfigParser parser("xritdemod.cfg");
+	float rrcAlpha;
+	uint32_t symbolRate, centerFrequency, sampleRate;
+	uint8_t lnaGain = 15, vgaGain = 15, mixerGain = 15;
+	bool agcEnable = false;
+
+	if (!parser.LoadFile()) {
+		// Add defaults to LRIT
+		std::cout << "No config file found. Defaulting to LRIT and creating config file." << std::endl;
+		std::cout << "The config file will be created with both \"mode\" and specific parameters. " << std::endl;
+		std::cout << "But if mode is specified, the other arguments will be ignore. The created file is just an example." << std::endl;
+		setDefaults(parser);
+	}
+
+	if (parser.hasKey(CFG_MODE)) {
+		if (parser[CFG_MODE] == "lrit") {
+			std::cout << "Selected LRIT mode. Ignoring parameters from config file." << std::endl;
+			setLRITMode(parser);
+		} else if (parser[CFG_MODE] == "hrit") {
+			std::cout << "Selected HRIT mode. Ignoring parameters from config file." << std::endl;
+			setHRITMode(parser);
+		} else {
+			std::cerr << "Invalid mode specified: " << parser[CFG_MODE] << std::endl;
+			return 1;
+		}
+	}
+
+	if (parser.hasKey(CFG_SYMBOL_RATE)) {
+		symbolRate = parser.getUInt(CFG_SYMBOL_RATE);
+	} else {
+		std::cerr << "Field \"symbolRate\" is missing on config file." << std::endl;
+		return 1;
+	}
+
+	if (parser.hasKey(CFG_RRC_ALPHA)) {
+		rrcAlpha = parser.getFloat(CFG_RRC_ALPHA);
+	} else {
+		std::cerr << "Field \"rrcAlpha\" is missing on config file." << std::endl;
+		return 1;
+	}
+
+	if (parser.hasKey(CFG_FREQUENCY)) {
+		centerFrequency = parser.getUInt(CFG_FREQUENCY);
+	} else {
+		std::cerr << "Field \"frequency\" is missing on config file." << std::endl;
+		return 1;
+	}
+
+	if (parser.hasKey(CFG_SAMPLE_RATE)) {
+		sampleRate = parser.getUInt(CFG_SAMPLE_RATE);
+	} else {
+		std::cerr << "Field \"sampleRate\" is missing on config file." << std::endl;
+		return 1;
+	}
+
+	if (parser.hasKey(CFG_DECIMATION)) {
+		baseDecimation = parser.getInt(CFG_DECIMATION);
+	} else {
+		std::cerr << "Field \"decimation\" is missing on config file." << std::endl;
+		return 1;
+	}
+
+	if (parser.hasKey(CFG_LNA_GAIN)) {
+		lnaGain = parser.getInt(CFG_LNA_GAIN);
+	}
+
+	if (parser.hasKey(CFG_MIXER_GAIN)) {
+		mixerGain = parser.getInt(CFG_MIXER_GAIN);
+	}
+
+	if (parser.hasKey(CFG_VGA_GAIN)) {
+		vgaGain = parser.getInt(CFG_VGA_GAIN);
+	}
+
+	if (parser.hasKey(CFG_AGC)) {
+		agcEnable = parser.getBool(CFG_AGC);
+		if (agcEnable) {
+			lnaGain = 15;
+			vgaGain = 15;
+			mixerGain = 15;
+		}
+	}
 
 	AirspyDevice::Initialize();
 	AirspyDevice airspy;
 
 	bool isMini = false;
+	bool sampleRateSet = false;
 
 	std::vector<uint32_t> sampleRates = airspy.GetAvailableSampleRates();
-	for (uint32_t sampleRate : sampleRates) {
-		if (sampleRate == 3000000) {
+	for (uint32_t asSR : sampleRates) {
+		if (asSR == sampleRate) {
 			isMini = true;
 			airspy.SetSampleRate(sampleRate);
-			break;
-		} else if (sampleRate == 2500000) {
-			isMini = false;
-			airspy.SetSampleRate(sampleRate);
+			sampleRateSet = true;
 			break;
 		}
+	}
+
+	if (!sampleRateSet) {
+		std::cerr << "Your device is not compatible with sampleRate \"" << sampleRate << "\"" << std::endl;
+		return 1;
 	}
 
 	std::cout << "Airspy " << (isMini ? "Mini " : "R2 ")
@@ -174,30 +258,40 @@ int main(int argc, char **argv) {
 
 	airspy.SetSamplesAvailableCallback(onSamplesAvailable);
 
-	float circuitSampleRate = airspy.GetSampleRate() / ((float) BASE_DECIMATION);
-	float sps = circuitSampleRate / ((float) SYMBOL_RATE);
+	float circuitSampleRate = airspy.GetSampleRate() / ((float) baseDecimation);
+	float sps = circuitSampleRate / ((float) symbolRate);
 
 	std::cout << "Samples per Symbol: " << sps << std::endl;
 	std::cout << "Circuit Sample Rate: " << circuitSampleRate << std::endl;
 	std::cout << "Low Pass Decimator Cut Frequency: " << circuitSampleRate / 2 << std::endl;
 
-	std::vector<float> rrcTaps = Filters::RRC(1, circuitSampleRate, SYMBOL_RATE, RRC_ALPHA, RRC_TAPS);
+	std::vector<float> rrcTaps = Filters::RRC(1, circuitSampleRate, symbolRate, rrcAlpha, RRC_TAPS);
 	std::vector<float> decimatorTaps = Filters::lowPass(1, airspy.GetSampleRate(), circuitSampleRate / 2, 100e3, FFTWindows::WindowType::HAMMING, 6.76);
 
-	decimator = new FirFilter(BASE_DECIMATION, decimatorTaps);
+	decimator = new FirFilter(baseDecimation, decimatorTaps);
 	agc = new AGC(AGC_RATE, AGC_REFERENCE, AGC_GAIN, AGC_MAX_GAIN);
 	costasLoop = new CostasLoop(PLL_ALPHA, LOOP_ORDER);
 	clockRecovery = new ClockRecovery(sps, CLOCK_GAIN_OMEGA, CLOCK_MU, CLOCK_ALPHA, CLOCK_OMEGA_LIMIT);
 	rrcFilter = new FirFilter(1, rrcTaps);
 
-	airspy.SetCenterFrequency(CENTER_FREQUENCY);
-	airspy.SetAGC(true);
-	airspy.SetMixerGain(15);
-	airspy.SetLNAGain(15);
-	airspy.SetVGAGain(15);
+	std::cout << "Center Frequency: " << (centerFrequency / 1000000.0) << " MHz" << std::endl;
+	std::cout << "Automatic Gain Control: " << (agcEnable ? "Enabled" : "Disabled") << std::endl;
+	if (!agcEnable) {
+		std::cout << "	LNA Gain: " << lnaGain << std::endl;
+		std::cout << "	VGA Gain: " << vgaGain << std::endl;
+		std::cout << "	MIX Gain: " << mixerGain << std::endl;
+	}
+	airspy.SetCenterFrequency(centerFrequency);
+	if (agcEnable) {
+		airspy.SetAGC(true);
+	} else {
+		airspy.SetAGC(false);
+		airspy.SetMixerGain(lnaGain);
+		airspy.SetLNAGain(vgaGain);
+		airspy.SetVGAGain(mixerGain);
+	}
 
 	std::cout << "Starting Airspy" << std::endl;
-	std::cout << "Center Frequency: " << airspy.GetCenterFrequency() << std::endl;
 	airspy.Start();
 	running = true;
 
